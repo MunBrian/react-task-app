1a2a5a0974d93fac5bf2992e800eb853eae1dae8
Brian Mungai <bryanmungai254@gmail.com>
2022-06-22 21:40:47 +0300

    initial commit

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+const About = () => {
+  return (
+    <div className="about">
+      <h4>Version 1.2.0 </h4>
+      <Link to="/">Go back</Link>
+    </div>
+  );
+};
+
+export default About;
